3d5516a2e482f6f815912fb0606d44cb4583401e
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-11-11 19:16:44 +0530

    kth max

---
+++ question10.c
@@ -0,0 +1,17 @@
+// kth max and min in araay
+#include<stdio.h>
+int main(){
+   int n;
+   printf("Enter the number of elements:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("\nEnter the elements:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("\nEnter the kth element:");
+    int k;
+    scanf("%d",&k);
+    
+
+}
